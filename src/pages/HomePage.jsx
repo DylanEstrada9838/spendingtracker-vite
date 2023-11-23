64d10183fbcce53fced781edc64424fe69c3c079
dylanestrada <dylaneb9861@gmail.com>
2023-11-22 22:41:20 -0600

    Added sign-in button in home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,10 @@
+import { Link } from "react-router-dom";
 const HomePage = () => {
   return (
     <div>
+       <Link to={`/sign-in`}>
+        <button>Sign-in</button>
+        </Link>
       <h1>HomePage</h1>
      
     </div>
